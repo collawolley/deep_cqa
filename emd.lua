1b1edad77c475458657e1bd671277e045e72b2d6
liangjz <accountset@163.com>
2015-12-14 21:41:24 +0800

    词嵌入载入这块完成，下面写一个简单的神经网络训练

--- emd.lua
+++ emd.lua
@@ -2,17 +2,37 @@
 	word vector reading layer
 --]]
 require('.')
-emd ={}
-local dict = deep_cqa.Vocab(deep_cqa.config.dict)
-local emd_vec = deep_cqa.config.emd_vec
-local emd_dict = deep_cqa.config.emb_dict
-emd_dict,emd_vec = emd.read_embedding(emd_dict,emd_vec)
-print(emd_dict)
-
------------------------------------------------------------
-function emd.read_embedding(vocab_path, emb_path)
-  local vocab = treelstm.Vocab(vocab_path)
+require('Vocab.lua')
+----------------------------------------------------------
+function deep_cqa.read_embedding(vocab_path, emb_path)
+  local vocab = deep_cqa.Vocab(vocab_path)
   local embedding = torch.load(emb_path)
   return vocab, embedding
 end
 ------------------------------------------------------------
+
+function deep_cqa.get_sub_embedding()
+	local dict = deep_cqa.Vocab(deep_cqa.config.dict)
+	local emd_vec = deep_cqa.config.emd_vec
+	local emd_dict = deep_cqa.config.emd_dict
+	local emd_dim =  deep_cqa.config.emd_dim
+	emd_dict,emd_vec =deep_cqa.read_embedding(emd_dict,emd_vec)
+	local vecs = torch.Tensor(dict.size,emd_dim)
+	local unseen_count = 0
+	for i =1, dict.size do
+	local w =  string.gsub(dict:token(i),'\\','')
+	print(w)
+		if emd_dict:contains(w) then
+			vecs[i] = emd_vec[emd_dict:index(w)]
+		else
+			unseen_count =unseen_count + 1
+			vecs[i]	= uniform(-0.05,0.05)
+		end
+	end
+	print('unseen words count:',unseen_count)
+	emd_vec = nil
+	emd_dict = nil
+	return dict,vecs
+end
+
+deep_cqa.get_sub_embedding()
