deb2a5014e0bbefd381e22bb9161c0889cc7a327
liangjz <accountset@163.com>
2015-12-14 20:41:56 +0800

    添加读取词向量代码

--- emd.lua
+++ emd.lua
@@ -2,7 +2,17 @@
 	word vector reading layer
 --]]
 require('.')
+emd ={}
 local dict = deep_cqa.Vocab(deep_cqa.config.dict)
-local emb_vec = deep_cqa.config.emd_vec
-local emb_dict = deep_cqa.config.emb_dict
-print(emb_vec)
+local emd_vec = deep_cqa.config.emd_vec
+local emd_dict = deep_cqa.config.emb_dict
+emd_dict,emd_vec = emd.read_embedding(emd_dict,emd_vec)
+print(emd_dict)
+
+-----------------------------------------------------------
+function emd.read_embedding(vocab_path, emb_path)
+  local vocab = treelstm.Vocab(vocab_path)
+  local embedding = torch.load(emb_path)
+  return vocab, embedding
+end
+------------------------------------------------------------
