82d6a7434cf144d546950ef3450f42edbfb3444f
liangjz <accountset@163.com>
2015-12-16 15:55:42 +0800

    完成数据集的读取和存储，正在处理生成合适格式的训练集

--- init.lua
+++ init.lua
@@ -16,14 +16,27 @@ config.emd_vec = config.parent_path .. 'data/glove/glove.840B.300d.th'
 config.emd_dict = config.parent_path .. 'data/glove/glove.840B.vocab'
 config.train_corpus = config.parent_path .. 'data/text.txt'
 config.dict = config.parent_path .. 'data/text.dict'
-
+------------
+config.insurance = {}
+config.insurance.train = config.parent_path .. 'data/insurance_qa/' .. 'train.txt'
+config.insurance.dev = config.parent_path .. 'data/insurance_qa/' .. 'dev.txt'
+config.insurance.test1 = config.parent_path .. 'data/insurance_qa/' .. 'test1.txt'
+config.insurance.test2 = config.parent_path .. 'data/insurance_qa/' .. 'test2.txt'
+config.insurance.answer = config.parent_path .. 'data/insurance_qa/' .. 'answer.txt'
+config.insurance.dict = config.parent_path .. 'data/insurance_qa/' .. 'dict.txt'
+config.insurance.binary = config.parent_path .. 'data/insurance_qa/' .. 'full_dataset.bin'
+config.insurance.negative_size = 100
+-------------
 config.emd_dim = 300
 config.batch_size = 10
 
 deep_cqa.config = config
+deep_cqa.insurance = {}
 -------------------------------
 include('util/vocab.lua')
 include('util/emd.lua')
 include('util/read_data.lua')
 include('simple_model/avg_emd.lua')
-
+deep_cqa.load_insurance_binary()
+include('util/read_data.lua')
+deep_cqa.insurance.generate_train_set()
--- util/read_data.lua
+++ util/read_data.lua
@@ -38,5 +38,104 @@ function deep_cqa.read_one_sentece(sent,vocab)
 	return vecs	--转换完毕返回转换后的向量
 end
 ---------------------------------------------------------
---读入一个语料库，并将其转换成为合适处理的格式
----------------------------------------------------------
+--针对保险数据QA数据集设计的读取函数
+function deep_cqa.insurance.load_train()
+	local ip = deep_cqa.config.insurance.train
+	local file = io.open(ip,'r')
+	local line
+	local train_set = {}
+	while true do
+		line = file:read()
+		if line == nil then
+			break
+		end
+		local list = line:split('\t')
+		table.insert(train_set,{list[1],list[2]:split(' ')})
+	end
+	file:close()
+	deep_cqa.insurance['train'] = train_set
+end
+-----------------------------------
+function deep_cqa.insurance.load_answer()
+	local ip = deep_cqa.config.insurance.answer
+	local file = io.open(ip,'r')
+	local line
+	local answer_set = {}
+	while true do
+		line = file:read()
+		if line == nil then
+			break
+		end
+		local list = line:split('\t')
+		answer_set[list[1]] = list[2]
+	end
+	deep_cqa.insurance['answer'] = answer_set
+	file:close()
+end
+---------------------------------------
+function deep_cqa.insurance.load_test(name)
+	local ip = nil
+	if name == 'dev' then
+		ip = deep_cqa.config.insurance.dev
+	end
+	if name == 'test1' then
+		ip = deep_cqa.config.insurance.test1
+	end
+	if name == 'test2' then
+		ip = deep_cqa.config.insurance.test2
+	end
+	if ip == nil then 
+		return nil
+	end
+
+	local file = io.open(ip,'r')
+	local line
+	local test_set = {}
+	while true do
+		line = file:read()
+		if line == nil then
+			break
+		end
+		local list = line:split('\t')
+		table.insert(test_set,{list[1]:split(' '),list[2],list[3]:split(' ')})
+	end
+	deep_cqa.insurance[name] = test_set
+	file:close()
+end
+---------------------------------------
+--载入保险数据的整体执行函数
+function deep_cqa.load_insurance_txt_dataset()
+	deep_cqa.insurance.load_train()
+	deep_cqa.insurance.load_answer()
+	deep_cqa.insurance.load_test('dev')
+	deep_cqa.insurance.load_test('test1')
+	deep_cqa.insurance.load_test('test2')
+end
+function deep_cqa.save_insurance_binary()
+	if deep_cqa.insurance == nil then
+		return nil
+	end
+	local op = deep_cqa.config.insurance.binary
+	torch.save(op,deep_cqa.insurance,'binary')
+end
+function deep_cqa.load_insurance_binary()
+	local ip = deep_cqa.config.insurance.binary
+	deep_cqa.insurance = torch.load(ip)
+end
+----------------------------------------
+--保险数据集，构建合适的训练数据和测试数据
+function deep_cqa.insurance.generate_train_set()
+	local nsize = deep_cqa.config.insurance.negative_size
+	local train = {}
+	local dataset = deep_cqa.insurance
+	for num,item in pairs(dataset['train']) do
+		local qst =item[1]
+		for i = 1,#item[2] do
+			local ta = dataset['answer'][item[2][i]]
+			
+		end
+	end
+end
+---------------
+function deep_cqa.insurance.generate_test_set()
+end
