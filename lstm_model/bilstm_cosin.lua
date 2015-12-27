b5768b6ccf9b9712ad047dd3f1bf1438f6731e6e
liangjz_102 <deepnet_102@608.com>
2015-12-27 12:08:47 +0800

    bilstm+cov+mlp架构完成，下一步写训练和测试代码

--- lstm_model/bilstm_cosin.lua
+++ lstm_model/bilstm_cosin.lua
@@ -10,8 +10,8 @@ cfg.dict = nil
 cfg.emd = nil
 cfg.dim = deep_cqa.config.emd_dim
 cfg.mem = 30
-cfg.batch  = 50
-deep_cqa.ins_meth.load_binary()
+cfg.batch  = deep_cqa.config.batch_size
+-- deep_cqa.ins_meth.load_binary()	--保险数据集，这里载入是为了获得测试集和答案
 -----------------------
 function get_index(sent)
 	--	获取一个句子的索引表达，作为整个模型的输入，可以直接应用到词向量层
@@ -24,56 +24,106 @@ function get_index(sent)
 	end
 	return deep_cqa.read_one_sentence(sent,cfg.dict)
 end
---------------------------
-function get_emd(index)
-	--	备用函数，输入词的index组，输出查询到的词向量
-	if cfg.emd == nil then 
-		get_index('good start')
-	end
-	return  cfg.emd:forward(index)
-end
 ------------------------------------------------------------------------
 function getlm()
 	get_index('today is')
-	local lm = {}
-	lm.emd = cfg.emd:cuda()
+	------------------------------------
+	local lstm = nn.Sequential()	--包装，顺序执行的网络
+	lstm:add(nn.Identity())	--原样送出
+	lstm:add(nn.SplitTable(1))	--tensor分割成为table，调整输出格式方便使用lstm
+	lstm:add(nn.BiSequencerLM(nn.FastLSTM(cfg.dim,cfg.mem)))
+	lstm:add(nn.JoinTable(1))
+	lstm:add(nn.View(-1,cfg.mem*2))	--双向的lstm输出维度是内部维度的2倍
 
-	lm.qlstm = nn.Sequential()
-	lm.qlstm:add(nn.Identity())
-	lm.qlstm:add(nn.SplitTable(1))
-	lm.qlstm:add(nn.Sequencer(nn.FastLSTM(cfg.dim,cfg.mem)))
-	lm.qlstm:add(nn.SelectTable(-1))
-	lm.qlstm:cuda()
-	
-	lm.tlstm = nn.Sequential()
-	lm.tlstm:add(nn.Identity())
-	lm.tlstm:add(nn.SplitTable(1))
-	lm.tlstm:add(nn.Sequencer(nn.FastLSTM(cfg.dim,cfg.mem)))
-	lm.tlstm:add(nn.SelectTable(-1))
-	lm.tlstm:cuda()
+-------------------------------------
+	--下面是cos sim的计算nn图
+	local m1 = nn.ConcatTable()
+	m1:add(nn.Identity())
+	m1:add(nn.Identity())
+	local m2 = nn.Sequential()
+	m2:add(m1)
+	m2:add(nn.CMulTable())
+	m2:add(nn.Sum(2))
+	m2:add(nn.Reshape(1))
+	local m3 = m2:clone()
+	local m4 = nn.ParallelTable()
+	m4:add(m2)
+	m4:add(m3)
+	local m5 = nn.Sequential()
+	m5:add(m4)
+	m5:add(nn.MM(false,true))
+	m5:add(nn.Sqrt())
+	local m6 = nn.ConcatTable():add(nn.MM(false,true)):add(m5)
+	local m7 = nn.Sequential():add(m6):add(nn.CDivTable())
+	local cosine = m7
+	-- cos 相似度模块构建完成
+------------------------------------
 
-	lm.flstm = nn.Sequential()
-	lm.flstm:add(nn.Identity())
-	lm.flstm:add(nn.SplitTable(1))
-	lm.flstm:add(nn.Sequencer(nn.FastLSTM(cfg.dim,cfg.mem)))
-	lm.flstm:add(nn.SelectTable(-1))
-	lm.flstm:cuda()
+	local cov = nn.Sequential()
+	cov:add(nn.Replicate(1))	--增加维度的功能，好不容易才发现
+	cov:add(nn.SpatialConvolution(1,20,3,3,1,1,2,2))	--input需要是3维tensor
+	cov:add(nn.SpatialAdaptiveMaxPooling(1,1))
+	cov:add(nn.Reshape(20))
+	cov:add(nn.Tanh())
 	
-	lm.sub = nn.CSubTable():cuda()
+	local mlp = nn.Sequential()	--一个简单的感知机
+	mlp:add(nn.Linear(20,3))
+	mlp:add(nn.Tanh())
+	mlp:add(nn.Linear(3,1))
+	mlp:add(nn.SoftSign())	--输出为归一化后的评分
+-------------------------------------
+	local lm = {}	--待返回的语言模型
+	lm.emd = cfg.emd:cuda()	--词嵌入部分
+-------------------------------
+	lm.qlstm = lstm:clone():cuda()	--问题部分的bilstm模型
+	lm.tlstm = lstm:clone():cuda()	--两种答案的bilstm模型
+	lm.flstm = lstm:clone():cuda()
+	share_params(lm.tlstm,lm.flstm)
+-------------------------------
+	lm.tq_cos = cosine:clone():cuda()	--这个模型内部没有参数，所以无需共享也是相同的
+	lm.fq_cos = cosine:clone():cuda() 
 	
-	lm.qrsp = nn.Reshape(cfg.mem,1):cuda()
-	lm.arsp = nn.Reshape(1,cfg.mem):cuda()
+	lm.tq_cov = cov:clone():cuda()	--卷积模型+全图最大值pooling
+	lm.fq_cov = cov:clone():cuda()
+	share_params(lm.tq_cov,lm.fq_cov)
+
+	lm.tq_mlp = mlp:clone():cuda()	--两个模型的多层感知机层
+	lm.fq_mlp = mlp:clone():cuda()
+	share_params(lm.tq_mlp,lm.fq_mlp)
 	
-	lm.mm = nn.MM():cuda()
-	lm.lin = nn.Linear(cfg.mem^2,1):cuda()
-	lm.cm = nn.Sequential()
-	lm.cm:add(lm.mm)
-	lm.cm:add(nn.Reshape(cfg.mem^2))
-	lm.cm:add(lm.lin)
-	lm.cm:add(nn.SoftSign())
-	lm.cm:cuda()
+	lm.tq = nn.Sequential():add(lm.tq_cos):add(lm.tq_cov):add(lm.tq_mlp):cuda()
+	lm.fq = nn.Sequential():add(lm.fq_cos):add(lm.fq_cov):add(lm.fq_mlp):cuda()
+--------------------------------
+	lm.sub = nn.PairwiseDistance(1):cuda()
 
 	return lm
+
+end
+function testlm()
+	local ml = getlm()
+	local index1 = get_index('today is a good day'):clone()
+	local index2 = get_index('today is a very good day'):clone()
+	local index3 = get_index('This class creates an output where the input is replicated'):clone()
+	local vec1 = ml.emd:forward(index1):clone()
+	local vec2 = ml.emd:forward(index2):clone()
+	local vec3 = ml.emd:forward(index3):clone()
+	local rep1 = ml.qlstm:forward(vec1)
+	local rep2 = ml.tlstm:forward(vec2)
+	local rep3 = ml.flstm:forward(vec3)
+	--print(rep1:size(),rep2:size(),rep3:size())
+	local cos1 = ml.tq_cos:forward({rep1,rep2})
+	local cos2 = ml.fq_cos:forward({rep1,rep3})
+	local cov1= ml.tq_cov:forward(cos1)
+	local cov2= ml.fq_cov:forward(cos2)
+	print(cov1,cov2)
+	print(ml.tq_mlp:forward(cov1))
+	print(ml.fq_mlp:forward(cov2))
+	print('-------------------')
+	local r5 = ml.tq:forward({rep1,rep2})
+	local r6 = ml.fq:forward({rep1,rep3})
+	print(r5,r6)
+	local sub = ml.sub:forward({r5,r6})
+	print(sub)
 end
 
 cfg.lm = getlm()
@@ -218,6 +268,8 @@ function evaluate(name)
 	local results = torch.Tensor(results)
 	print(torch.sum(results,1)/results:size()[1])
 end
-train()
-evaluate('dev')
+getlm()
+testlm()
+--train()
+--evaluate('dev')
 
